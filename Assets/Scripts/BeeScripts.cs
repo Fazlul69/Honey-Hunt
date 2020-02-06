9b07fb9c9ef854797a4d32d2c7587772a9143bdf
Fazlul69 <sawravm299@gmail.com>
2020-02-06 15:46:27 +0600

    upload

---
+++ Assets/Scripts/BeeScripts.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeScripts : MonoBehaviour
+{
+    public Transform[] combPoints;
+    private float moveSpeed = 20f;
+    private float step;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BeeMovement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = moveSpeed;
+    }
+
+    IEnumerator BeeMovement()
+    {
+        //int beePos = Random.Range(0, combPoints.Length);
+        for (int i = 0; i < combPoints.Length; i++) {
+            transform.position = Vector2.MoveTowards(transform.position, combPoints[i].position, step);
+            yield return new WaitForSeconds(3f);
+        }
+        //yield return new WaitForSeconds(3);
+        //StartCoroutine(BeeMovement());
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Camera myCam;
+    private Vector3 clickPos;
+    private float moveSpeed = 10;
+    float step;
+    private Vector2 target;
+    private SpriteRenderer sr;
+    [SerializeField]
+    private Image fillImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myCam = Camera.main;
+        sr = GetComponent<SpriteRenderer>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //clickPos = myCam.ScreenToWorldPoint(Input.mousePosition);
+
+        step = moveSpeed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPos = myCam.ScreenToWorldPoint(Input.mousePosition);
+           
+        }
+        target = clickPos;
+        movePlayer();
+
+        if(clickPos.x > transform.position.x)
+        {
+            sr.flipX = true;
+        }
+        if (clickPos.x < transform.position.x)
+        {
+            sr.flipX = false;
+        }
+        
+    }
+
+
+   void movePlayer()
+    {
+       transform.position = Vector2.MoveTowards(transform.position, target, step);  
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Comb")
+        {
+            
+            fillImage.fillAmount += 0.1f;
+            Destroy(col.gameObject,3f);
+        }
+    }
+   
+}
